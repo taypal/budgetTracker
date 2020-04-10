69f73cbfffda1c8f9ab92bb380aacead60c9dc54
taypal <tay.houk@yahoo.com>
2020-04-09 21:07:09 -0600

    added service worker and webmanifest

---
+++ public/service-worker.js
@@ -0,0 +1,71 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/styles.css",
+    "/index.js",
+
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+    if (event.request.url.startsWith(self.location.origin)) {
+        event.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(event.request)
+                    .then(response => {
+                        if (response.status === 200) {
+                            cache.put(event.request.url, response.clone());
+                        }
+
+                        return response;
+                    })
+                    .catch(err => {
+                        return cache.match(event.request);
+                    });
+            }).catch(err => console.log(err))
+        );
+        return;
+
+    }
+});
+
+event.respondWith(
+    fetch(event.request).catch(function () {
+        return caches.match(event.request).then(function (response) {
+            if (response) {
+                return response;
+
+            } else if (event.request.headers.get("accept").includes("text/html")) {
+                return caches.match("/");
+            }
+        });
+    })
+);
